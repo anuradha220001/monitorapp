be7dba7a183e9f0bc0710982126839a2b0cec9e7
anuradha220001 <118510672+anuradha220001@users.noreply.github.com>
2022-12-06 16:39:49 +0200

    Update Monitor.java
    default constructor

--- Monitor.java
+++ Monitor.java
@@ -1,13 +1,16 @@
 package monitor;
 
 public class Monitor {
+	
 	public boolean isOn;
 	public String brand;
  
+	//default constructor
 	public Monitor() {
 		isOn = false;
 		brand = "";
 	}
+	//parameterised constructor
 	public Monitor(boolean On, String monitorBrand) {
 		isOn = false;
 		brand = monitorBrand;
